04281c132eb06aa07432e56d56198f4be3ba7936
atomspace <atomspace@gmail.com>
2018-09-17 20:24:19 +0300

    Included .css file. Input for game size.

--- index.js
+++ index.js
@@ -2,3 +2,20 @@
 var canvas  = document.getElementById("gamezone");
 var context = canvas.getContext("2d");
 var body    = document.querySelector("body");
+
+//initializing inputs for game properties
+let inputGameSize = document.getElementById("inputGameSize");
+
+//functions to change game
+var changeGameSize = () => {
+    let size = inputGameSize.value;
+    if(size < 60 && size > 5) {
+        canvas.width  = size * 20;
+        canvas.height = size * 20;
+    } else {
+        if(size > 60) {
+            canvas.width  = 60 * 20;
+            canvas.height = 60 * 20;
+        }
+    }
+}
